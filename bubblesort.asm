957e0d1f810572ffc40074f39d1375deea4f3e08
Shreenivas Jagdale <160756902+5iriJags@users.noreply.github.com>
2024-02-27 21:13:29 +0530

    Add files via upload

---
+++ bubblesort.asm
@@ -0,0 +1,32 @@
+org 100h
+mov si,1100h
+mov cl,09
+mov ch,09
+mov al,[si]
+inc si
+
+up: cmp al,[si] 
+jc big
+mov bl,[si]
+mov [si],al
+dec si
+mov [si],bl 
+inc si
+inc si
+dec ch
+jnz up
+jmp outer
+
+big: mov al,[si]
+inc si
+dec ch
+jnz up
+ 
+outer: mov si,1100h 
+mov al,[si]
+inc si
+mov ch,09
+dec cl
+jnz up
+hlt
+
